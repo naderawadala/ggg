7e7807cad863ed5f07c74ff15a7c857091a046a9
Nader <naderawadala@gmail.com>
2020-03-19 18:11:36 +0200

    init

---
+++ WpfApp1/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+	public class Converter
+	{
+		private int decimalBase = 10;
+		private int binaryBase = 2;
+		private int octBase = 8;
+		private int hexBase = 16;
+		public string ConvertToBinary(string number)
+		{
+		return  Convert.ToString(Convert.ToInt32(number, decimalBase), binaryBase);
+		}
+		public string ConvertToOctal(string number)
+		{
+			return Convert.ToString(Convert.ToInt32(number, decimalBase), octBase);
+		}
+		public string ConvertToHexadecimal(string number)
+		{
+			return Convert.ToString(Convert.ToInt32(number, decimalBase), hexBase);
+		}
+	}
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			
+		}
+
+		private void binaryBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Converter converter = new Converter();
+			resLbl.Content =converter.ConvertToBinary(input.Text);
+		}
+
+		private void octalBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Converter converter = new Converter();
+			resLbl.Content = converter.ConvertToOctal(input.Text);
+		}
+
+		private void hexadicimalBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Converter converter = new Converter();
+			resLbl.Content = converter.ConvertToHexadecimal(input.Text);
+		}
+	}
+}
